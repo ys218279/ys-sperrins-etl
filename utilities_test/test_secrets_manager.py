574c778332b69e911cafb8a01781b8394e697435
Angelo Rohanathan <angelorohanathan3@gmail.com>
2025-02-25 14:17:42 +0000

    Added the testing aws credentials to test secrets manager file

--- utilities_test/test_secrets_manager.py
+++ utilities_test/test_secrets_manager.py
@@ -4,6 +4,14 @@ import boto3
 from unittest.mock import patch
 from moto import mock_aws
 import io
+import os
+
+@pytest.fixture
+def aws_credentials():
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "eu-west-2"
 
 def input_args():
     yield "bidenj"
