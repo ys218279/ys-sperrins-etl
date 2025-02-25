94524191fbb78ec785941bfaf79de5215a47b5bb
ys218279 <ys218279@gmail.com>
2025-02-25 10:55:39 +0000

    added message explaining how the tfstate bucket works and naming convention to use

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,12 @@ terraform {
       version = "~> 5.0"
     }
   }
-  backend "s3"{
+  # Note with the backend bucket bellow is for the centralized version.
+  # When making tests or working locally you must adjust the name and make a bucket on your personal console for it to work
+
+    backend "s3"{
+    #test bucket name format should be:
+    # <Your name>-sperrins-data-bucket
     bucket = "sperrins-data-bucket"
     key = "tf_state/project_sperrins.tfstate"
     region = "eu-west-2"
