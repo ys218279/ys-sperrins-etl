ddcea4df4d883a8a044882db97aeb1034fb336b0
sgoksan <sgoksan4@gmail.com>
2025-02-25 12:22:44 +0000

    add initial templates for iam trust policies. Ready for others to add their own policies and attachements. Also includes an update to the .gitignore to ignore .terraform dirs and files.

---
+++ terraform/vars.tf
@@ -0,0 +1,24 @@
+variable "ingestion_lambda" {
+  type    = string
+  default = "ingestion_lambda"
+}
+
+variable "transform_lambda" {
+  type    = string
+  default = "transform_lambda"
+}
+
+variable "load_lambda" {
+  type    = string
+  default = "transform_lambda"
+}
+
+variable "state_machine_scheduler" {
+  type    = string
+  default = "state_machine_scheduler"
+}
+
+variable "python_runtime" {
+  type    = string
+  default = "python3.12"
+}
