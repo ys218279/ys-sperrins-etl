c2bf0f8479da893830fda5b0aec61048525b53a9
ys218279 <ys218279@gmail.com>
2025-02-27 14:58:50 +0000

    added aws cloudwatch metric alarm for ingestion lamda, created Sns topic added variable

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -2,3 +2,21 @@ resource "aws_cloudwatch_log_group" "example" {
   name              = "/aws/lambda/${var.ingestion_lambda}"
   retention_in_days = 14
 }
+
+
+resource "aws_cloudwatch_metric_alarm" "ingestion_lambda_alarm" {
+  alarm_name          = "Ingestion-lambda-failure"
+  alarm_description   = "Triggers when ingestion lambda fails."
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 1
+  alarm_actions       = [aws_sns_topic.team_sperrins_topic.arn]
+
+  dimensions = {
+    FunctionName = aws_lambda_function.ingestion_lambda.function_name
+  }
+}
--- terraform/sns.tf
+++ terraform/sns.tf
@@ -1,14 +1,12 @@
 # Create an SNS topic
-resource "aws_sns_topic" "lambda_Ingestion_topic" {
-  name = var.lambda_Ingestion_topic_name 
+resource "aws_sns_topic" "team_sperrins_topic" {
+  name = var.lambda_ingestion_topic_name 
 }
 
 
 #havent figured out names
-resource "aws_sns_topic_subscription" "lambda_Ingestion_topic_target" {
-  topic_arn = aws_sns_topic.lambda_Ingestion_topic.arn
+resource "aws_sns_topic_subscription" "team_sperrins_topic" {
+  topic_arn = aws_sns_topic.team_sperrins_topic.arn
   protocol  = "email"
   endpoint  = var.email_address
 }
-// run terraform first 
-// sns topic -> subscribe
