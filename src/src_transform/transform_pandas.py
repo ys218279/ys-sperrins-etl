63f02ac7be1a09a659864324731db7312cabf21e
Lcarriersippy <dfclcs@gmail.com>
2025-03-05 09:46:01 +0000

    add docstrings for all transform lambda util functions

--- src/src_transform/transform_pandas.py
+++ src/src_transform/transform_pandas.py
@@ -4,11 +4,27 @@ import json
 
 
 def create_dim_design_table(df_des):
+    """Converts source df table to target df table schema
+    
+    Keyword arguments:
+    - df_des (panda df obj): The table in dataframe format - source
+    
+    Returns:
+    - df_dim_design (panda df obj): The table in dataframe format - target
+    """
     df_design = df_des.copy().set_index('design_id')
     df_dim_design = df_design.drop(columns=['created_at','last_updated'])
     return df_dim_design
 
 def create_dim_currency_table(df_cur):
+    """Converts source df table to target df table schema
+    
+    Keyword arguments:
+    - df_cur (panda df obj): The table in dataframe format - source
+    
+    Returns:
+    - df_dim_currency (panda df obj): The table in dataframe format - target
+    """
     df_currency = df_cur.copy().set_index('currency_id')
     df_dim_currency = df_currency.drop(columns=['created_at','last_updated'])
     currency_dictionary = {'GBP': 'Pound sterling', 'EUR': 'Euro', 'USD': 'United States dollar'}
@@ -16,6 +32,15 @@ def create_dim_currency_table(df_cur):
     return df_dim_currency
 
 def create_dim_staff_table(df_sta, df_dep):
+    """Converts two source df table to one target df table schema
+    
+    Keyword arguments:
+    - df_sta (panda df obj): The table in dataframe format - source
+    - df_dep (panda df obj): The table in dataframe format - source
+    
+    Returns:
+    - df_dim_staff (panda df obj): The table in dataframe format - target
+    """
     df_staff = df_sta.copy()
     df_department = df_dep.copy()
     df_staff_mod = df_staff.drop(columns=['created_at','last_updated'])
@@ -25,6 +50,14 @@ def create_dim_staff_table(df_sta, df_dep):
     return df_dim_staff
 
 def create_dim_location_table(df_addr):
+    """Converts source df table to target df table schema
+    
+    Keyword arguments:
+    - df_addr (panda df obj): The table in dataframe format - source
+    
+    Returns:
+    - df_dim_location_mod_2 (panda df obj): The table in dataframe format - target
+    """
     df_address = df_addr.copy()
     df_dim_location = df_address.drop(columns=['created_at','last_updated'])
     df_dim_location_mod = df_dim_location.rename(columns={'address_id':'location_id'})
@@ -32,6 +65,15 @@ def create_dim_location_table(df_addr):
     return df_dim_location_mod_2
 
 def create_dim_counterparty_table(df_addr, df_cp):
+    """Converts two source df table to one target df table schema
+    
+    Keyword arguments:
+    - df_addr (panda df obj): The table in dataframe format - source
+    - df_cp (panda df obj): The table in dataframe format - source
+    
+    Returns:
+    - df_dim_counterparty_mod_2 (panda df obj): The table in dataframe format - target
+    """
     df_address = df_addr.copy()
     df_counterparty = df_cp.copy()
     df_address_mod = df_address.drop(columns=['created_at','last_updated'])
@@ -49,9 +91,14 @@ def create_dim_counterparty_table(df_addr, df_cp):
 
 def create_dim_date_table(start='2019/01/01', end='2030/12/31'):
     '''
-    Create Dimension Date in Pandas
+    Creates Dimension Date in Pandas
+
+    Keyword arguments:
+    - start (str): The start date in string format
+    - end (str): The end date in string format
     
-    :return df_date : DataFrame
+    Returns:
+    - df_date_mod_2 (panda df obj): The table in dataframe format - target
     '''
     # Construct DIM Date Dataframe
     df_date = pd.DataFrame({"Date": pd.date_range(start=f'{start}', end=f'{end}')})
@@ -68,6 +115,14 @@ def create_dim_date_table(start='2019/01/01', end='2030/12/31'):
     return df_date_mod_2
 
 def create_fact_sales_order_table(df_sales):
+    """Converts source df table to target df table schema
+    
+    Keyword arguments:
+    - df_sales (panda df obj): The table in dataframe format - source
+    
+    Returns:
+    - df_fact_sales_order (panda df obj): The table in dataframe format - target
+    """
     df_sales_order = df_sales.copy()
     df_sales_order["created_date"] = pd.to_datetime(df_sales_order["created_at"], format='ISO8601').dt.strftime('%Y-%m-%d')
     df_sales_order["created_time"] = pd.to_datetime(df_sales_order["created_at"], format='ISO8601').dt.strftime('%H:%M:%S.%f')
