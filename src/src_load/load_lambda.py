a081097b4f6e6c101bbadbf54e65edf33559509b
Chiiiheng <jchjiangcheng@outlook.com>
2025-03-07 10:50:54 +0000

    added doc string to all functions, added tests for get_insert_query function

--- src/src_load/load_lambda.py
+++ src/src_load/load_lambda.py
@@ -23,7 +23,26 @@ sys.path.append("src/src_load")
 BUCKET_NAME = os.environ["S3_BUCKET_NAME_PROCESSED"]
 
 
+
+
+
 def lambda_handler(event, context, BUCKET_NAME=BUCKET_NAME):
+    """Main handler - This loads the data from the processed s3 bucket, into the
+                    required schema for the Data Warehouse
+      Args:
+            Event: {
+                    "dim_design": "dim_design/2025-03-06/224349.parquet",
+                    "dim_currency": False
+                    "dim_staff": "dim_staff/2025-03-06/224349.parquet",
+                    "dim_location": "dim_location/2025-03-06/224349.parquet",
+                    "dim_counterparty": "dim_counterparty/2025-03-06/224349.parquet",
+                    "dim_date": "dim_date/2025-03-06/224349.parquet",
+                    "fact_sales_order": "fact_sales_order/2025-03-06/224349.parquet"
+                    }   
+                        event is a dictionary that is passed in by StateMachine payload 
+                        with table names as keys, and object keys as values (False if no new table).
+            Context: supplied by AWS
+    """
     if event:
         conn = connect_to_dw()
         s3_client = get_s3_client()
