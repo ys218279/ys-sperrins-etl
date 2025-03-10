2da6b2e2140a21aa44eba9d5e756fee59dfd2428
Lcarriersippy <dfclcs@gmail.com>
2025-03-10 15:17:29 +0000

    add error and info logging to main ingest lambda handler, includes full TDD

--- src/src_ingestion/ingestion_lambda.py
+++ src/src_ingestion/ingestion_lambda.py
@@ -1,4 +1,5 @@
 import os
+import logging
 from datetime import datetime
 from pg8000.native import identifier
 from utils import (
@@ -13,6 +14,9 @@ from utils import (
 import sys
 
 sys.path.append("src/src_ingestion")
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
 
@@ -45,40 +49,55 @@ def lambda_handler(event, context, BUCKET_NAME=BUCKET_NAME):
         - Any data in a datetime format when extracted from ToteSys is coverted in to a integer format.
         - Within the uploaded file: any data fields that had None have been converted to 'null' values.
     """
-
-    conn = connect_to_db()
-    client = get_s3_client()
-    table_list = ['design', 'transaction', 'sales_order', 'address', 'counterparty', 'staff', 'purchase_order', 'payment', 'payment_type', 'currency', 'department']
+    conn = None
     output = {}
-    for table in table_list:
-        latest_update_s3 = fetch_latest_update_time_from_s3(client, BUCKET_NAME, table)
-        latest_update_db = fetch_latest_update_time_from_db(conn, table)
-        if latest_update_db > latest_update_s3:
-            latest_update_s3_dt = datetime.strptime(
-                str(latest_update_s3), "%Y%m%d%H%M%S"
-            )
-            raw_data = conn.run(
-                f"SELECT * FROM {identifier(table)} WHERE last_updated > :latest_update_s3_dt",
-                latest_update_s3_dt=latest_update_s3_dt,
-            )
-            columns = [col["name"] for col in conn.columns]
-            result = {"columns": columns, "data": raw_data}
-            object_name = upload_to_s3(BUCKET_NAME, table, result, client)
-            output[table] = object_name
-        else:
-            output[table] = False
+    try:
+        conn = connect_to_db()
+        client = get_s3_client()
+        table_list = ['design', 'transaction', 'sales_order', 'address', 'counterparty', 'staff', 'purchase_order', 'payment', 'payment_type', 'currency', 'department']
+        for table in table_list:
+            latest_update_s3 = fetch_latest_update_time_from_s3(client, BUCKET_NAME, table)
+            latest_update_db = fetch_latest_update_time_from_db(conn, table)
+            if latest_update_db > latest_update_s3:
+                latest_update_s3_dt = datetime.strptime(
+                    str(latest_update_s3), "%Y%m%d%H%M%S"
+                )
+                raw_data = conn.run(
+                    f"SELECT * FROM {identifier(table)} WHERE last_updated > :latest_update_s3_dt",
+                    latest_update_s3_dt=latest_update_s3_dt,
+                )
+                columns = [col["name"] for col in conn.columns]
+                result = {"columns": columns, "data": raw_data}
+                object_name = upload_to_s3(BUCKET_NAME, table, result, client)
+                if object_name:
+                    output[table] = object_name
+                    logger.info("Wrote %s table to S3", str(table))
+                else:
+                    logger.info("There was a problem. %s table not written to S3.", str(table))
+            else:
+                output[table] = False
 
-    # Fetch full snapshot for cases where a table in DW needs a join, so it needs data from its counterpart table 
-    table_joins_lookup = {'staff':'department', 
-                          'department':'staff', 
-                          'counterparty':'address', 
-                          'address':'counterparty'}
-    list_tables_needed = [value for key,value in table_joins_lookup.items() if output[key]]
-    
-    for table_name in list_tables_needed:
-        result = fetch_snapshot_of_table_from_db(conn, table_name)
-        object_name = upload_to_s3(BUCKET_NAME, table_name, result, client)
-        output[table_name] = object_name
+        # Fetch full snapshot for cases where a table in DW needs a join, so it needs data from its counterpart table 
+        table_joins_lookup = {'staff':'department', 
+                            'department':'staff', 
+                            'counterparty':'address', 
+                            'address':'counterparty'}
+        list_tables_needed = [value for key,value in table_joins_lookup.items() if output[key]]
+        
+        for table_name in list_tables_needed:
+            result = fetch_snapshot_of_table_from_db(conn, table_name)
+            object_name = upload_to_s3(BUCKET_NAME, table_name, result, client)
+            if object_name:
+                output[table_name] = object_name
+                logger.info("Wrote %s table to S3", str(table_name))
+            else:
+                logger.info("There was a problem. %s table not written to S3.", str(table_name))
 
-    close_db_connection(conn)
-    return output
+        close_db_connection(conn)
+        return output
+    except Exception as e:
+        logger.info("Unexpected Exception: %s", str(e))
+        return output
+    finally:
+        if conn:
+            close_db_connection(conn)
