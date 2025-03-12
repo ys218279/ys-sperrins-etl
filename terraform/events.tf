48afcab5459a87b45bb66c7610bc65e01cb962ed
Lcarriersippy <dfclcs@gmail.com>
2025-03-12 07:11:00 +0000

    make the statemachine asl file fully dynamic

--- terraform/events.tf
+++ terraform/events.tf
@@ -1,15 +1,17 @@
-# eventbridge scheduler trigggers/invokes the stepfunction every 5(?) minutes
-# the stepfunction invokes the first lambda (no payload entering?)
-# the lambda runs and dumps to s3 (exit payload success message?)
-# the payload from lambda 1 is passed to lambda 2 as payload, triggering lambda 2
+data "template_file" "SFDefinitionFile" {
+  template = file("${path.module}/mystatemachine.asl.json")
+  vars = {
+    LambdaFunctionIngest    = aws_lambda_function.ingestion_lambda.arn,
+    LambdaFunctionTransform = aws_lambda_function.transform_lambda.arn,
+    LambdaFunctionLoad      = aws_lambda_function.load_lambda.arn
+  }
+}
 
 resource "aws_sfn_state_machine" "pipeline_state_machine" {
-  name     = var.state_machine
-  role_arn = aws_iam_role.state_machine_role.arn
-  definition = templatefile("${path.module}/mystatemachine.asl.json", {
-    ProcessingLambda = "arn:aws:lambda:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:function:${var.ingestion_lambda}:$LATEST"
-    }
-  )
+  name       = var.state_machine
+  role_arn   = aws_iam_role.state_machine_role.arn
+  definition = data.template_file.SFDefinitionFile.rendered
+  type       = "STANDARD"
   depends_on = [aws_lambda_function.ingestion_lambda]
 }
 
